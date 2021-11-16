390c6a2c1c21c001337fe1c29e723ad30bb751dc
Jeffrey Martinez <martinezjf2@gmail.com>
2021-11-16 12:33:56 -0500

    tested the question width

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
 // eslint-disable-next-line
 const API_URL = 'https://inter-questionss.herokuapp.com'
 
 function App() {
+  const [questions, setQuestions] = useState([])
+
+
+
+
   return (
     <div className="container">
       <div className="bg-white text-blue-800 p-10 rounded-lg shadow-md">
-        <h2 className="text-2xl"> 1. This is a question?</h2>
+        <h2 className="text-2xl"> 1. This is a question? kjdcnv di j iodv isnv ojnvo in sv n ov onsv oinvo inos fj j jg jfsgbkjngbjnsf osfg of sfg oimsg oimsfg if oimsg o oimg of oif oimid oih oijgd </h2>
       </div>
-      <div className="mt-4 flex flex-wrap justify-around">
-        <button className="bg-white w-5/12 rounded shadow p-4 font-semibold text-blue-800">
+      <div className="grid grid-cols-2 gap-6 mt-4">
+        <button className="bg-white rounded shadow p-4 font-semibold text-blue-800">
           Answer 1
         </button>
-        <button className="bg-white w-5/12 rounded shadow p-4 font-semibold text-blue-800">
+        <button className="bg-white rounded shadow p-4 font-semibold text-blue-800">
           Answer 2
         </button>
-        <button className="bg-white w-5/12 rounded shadow p-4 font-semibold text-blue-800 mt-4">
+        <button className="bg-white rounded shadow p-4 font-semibold text-blue-800">
           Answer 3
         </button>
-        <button className="bg-white w-5/12 rounded shadow p-4 font-semibold text-blue-800 mt-4">
+        <button className="bg-white rounded shadow p-4 font-semibold text-blue-800">
           Answer 4
         </button>
         
