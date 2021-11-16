632f8cba61732dc4158921e98b082610a51695df
Jeffrey Martinez <martinezjf2@gmail.com>
2021-11-16 12:22:51 -0500

    added more styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from "react";
 
+// eslint-disable-next-line
 const API_URL = 'https://inter-questionss.herokuapp.com'
 
 function App() {
   return (
     <div className="container">
       <div className="bg-white text-blue-800 p-10 rounded-lg shadow-md">
-        <h2 className="text-2xl">This is a question</h2>
+        <h2 className="text-2xl"> 1. This is a question?</h2>
       </div>
-      <div>
-        <button className="bg-white w-1/2 p-4 text-blue-800">
+      <div className="mt-4 flex flex-wrap justify-around">
+        <button className="bg-white w-5/12 rounded shadow p-4 font-semibold text-blue-800">
           Answer 1
         </button>
-        <button className="bg-white w-1/2 p-4 text-blue-800">
+        <button className="bg-white w-5/12 rounded shadow p-4 font-semibold text-blue-800">
           Answer 2
         </button>
-        <button className="bg-white w-1/2 p-4 text-blue-800">
+        <button className="bg-white w-5/12 rounded shadow p-4 font-semibold text-blue-800 mt-4">
           Answer 3
         </button>
-        <button className="bg-white w-1/2 p-4 text-blue-800">
+        <button className="bg-white w-5/12 rounded shadow p-4 font-semibold text-blue-800 mt-4">
           Answer 4
         </button>
         
