8541f1206e5a95571debf851e9e8838902c74a0c
Jeffrey Martinez <martinezjf2@gmail.com>
2021-11-16 19:51:18 -0500

    added the answer prop and the handleAnswer event

--- src/components/Questionaire.js
+++ src/components/Questionaire.js
@@ -9,21 +9,22 @@ function Button({answer}) {
 
 }
 
-function Questionaire({data: {question, correct_answer, incorrect_answers}}) {
+function Questionaire({ handleAnswer, data: {question, correct_answer, incorrect_answers}}) {
+
+    const shuffledAnswer = [correct_answer, ...incorrect_answers]
+
     return (
         <div>
         <div className="bg-white text-blue-800 p-10 rounded-lg shadow-md">
         <h2 className="text-2xl">{question}</h2>
-      </div>
-      <div className="grid grid-cols-2 gap-6 mt-6">
-        <Button answer={correct_answer}/>
-        <Button answer={incorrect_answers[0]}/>
-        <Button answer={incorrect_answers[1]}/>
-        <Button answer={incorrect_answers[2]}/>
-
-      </div>
-      </div>
-    )
-}
-
-export default Questionaire
+        </div>
+        <div className="grid grid-cols-2 gap-6 mt-6">
+            <Button answer={shuffledAnswer[0]} onClick={() => handleAnswer(shuffledAnswer[0])} />
+            <Button answer={shuffledAnswer[1]} onClick={() => handleAnswer(shuffledAnswer[1])} />
+            <Button answer={shuffledAnswer[2]} onClick={() => handleAnswer(shuffledAnswer[2])}  />
+            <Button answer={shuffledAnswer[3]} onClick={() => handleAnswer(shuffledAnswer[3])}  />
+        </div>
+        </div>   
+        )
+    } 
+    export default Questionaire
