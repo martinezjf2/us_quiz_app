628bba0d55d7f2ba30c3132b0298c9fe049b0f01
Jeffrey Martinez <martinezjf2@gmail.com>
2021-11-16 19:33:30 -0500

    created a button component to handle the arranging of the answers

--- src/components/Questionaire.js
+++ src/components/Questionaire.js
@@ -1,5 +1,14 @@
 import React from "react";
 
+function Button({answer}) {
+    return (
+        <button className="bg-white rounded shadow p-4 font-semibold text-blue-800">
+          {answer}
+        </button>
+    )
+
+}
+
 function Questionaire({data: {question, correct_answer, incorrect_answers}}) {
     return (
         <div>
@@ -7,19 +16,11 @@ function Questionaire({data: {question, correct_answer, incorrect_answers}}) {
         <h2 className="text-2xl">{question}</h2>
       </div>
       <div className="grid grid-cols-2 gap-6 mt-6">
-        <button className="bg-white rounded shadow p-4 font-semibold text-blue-800">
-          {correct_answer}
-        </button>
-        <button className="bg-white rounded shadow p-4 font-semibold text-blue-800">
-          {incorrect_answers[0]}
-        </button>
-        <button className="bg-white rounded shadow p-4 font-semibold text-blue-800">
-          {incorrect_answers[1]}
-        </button>
-        <button className="bg-white rounded shadow p-4 font-semibold text-blue-800">
-          {incorrect_answers[2]}
-        </button>
-        
+        <Button answer={correct_answer}/>
+        <Button answer={incorrect_answers[0]}/>
+        <Button answer={incorrect_answers[1]}/>
+        <Button answer={incorrect_answers[2]}/>
+
       </div>
       </div>
     )
